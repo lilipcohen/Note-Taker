6834c724fffa2e89105594ca3c3e164bb7d9bf0b
Lili Cohen <lili.cohen.tv@gmail.com>
2021-01-01 17:04:57 -0700

    adding different routes

--- Develop/server.js
+++ Develop/server.js
@@ -11,11 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.get("/api/notes", function(req, res) {
