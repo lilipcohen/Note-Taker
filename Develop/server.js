40b40dfe81afbc06cacbf8339b6a8142cbc1881c
Lili Cohen <lili.cohen.tv@gmail.com>
2021-01-02 18:34:33 -0700

    changing const port to var

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const notes = require("./db/db.json");
 
 const app = express();
-const PORT = process.env.PORT || 8080; 
+var PORT = process.env.PORT || 8080; 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
